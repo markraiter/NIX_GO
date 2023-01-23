9f38e68fe995ac137ce91002d085dbab93135a02
MSRBanker <96476554+MSRBanker@users.noreply.github.com>
2023-01-23 09:58:07 +0200

    Initial commit with the simple get request

---
+++ main.go
@@ -0,0 +1,25 @@
+// GET REQUEST FROM https://jsonplaceholder.typicode.com/
+
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+)
+
+func makeRequest()  {
+	url := "https://jsonplaceholder.typicode.com/posts"
+
+	resp, err := http.Get(url)
+	if err != nil {panic(err)}
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {panic(err)}
+
+	fmt.Println(string(body))
+}
+
+func main()  {
+	makeRequest()
+}

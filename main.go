bf56d1d5654e414ee1b3cda2935e1903e14a22b4
MSRBanker <96476554+MSRBanker@users.noreply.github.com>
2023-01-23 12:39:10 +0200

    added get request using concurrency

--- main.go
+++ main.go
@@ -6,20 +6,33 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"strconv"
+	"time"
 )
 
-func makeRequest()  {
-	url := "https://jsonplaceholder.typicode.com/posts"
+var url string = "https://jsonplaceholder.typicode.com/posts/"
+var c chan string
+var result string
 
+func makeRequest(url string) {
 	resp, err := http.Get(url)
 	if err != nil {panic(err)}
 
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {panic(err)}
 
-	fmt.Println(string(body))
+	defer resp.Body.Close()
+
+	result = string(body)
+
+	fmt.Println(result)
 }
 
 func main()  {
-	makeRequest()
+	// makeRequest(url)
+	// GET REQUEST FROM https://jsonplaceholder.typicode.com/ USING CONCURRENCY (GOROUTINES)
+	for i := 1; i <= 100; i++ {
+		go makeRequest(url + strconv.Itoa(i))
+	}
+	time.Sleep(2 * time.Second)	
 }
